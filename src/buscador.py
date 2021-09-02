4620d604bc0dea6c3f0bf8772ef744ebdefe3dce
weebtor <victor.sanchez1@mail.udp.cl>
2021-09-02 06:41:17 +0000

    refactorizacion

--- src/buscador.py
+++ src/buscador.py
@@ -8,11 +8,11 @@ def hello():
 
 @app.route("/inventory/search")
 def search_inventory():
-    search_query = request.args.get("q")
-    cache_items = cache.cache_load_json(key=search_query)
+    search_query = request.args.get("q")                    # obtiene el parametro q
+    cache_products = cache.cache_load_products(key=search_query)        # busca en el cache
 
-    if cache_items:
-        return jsonify({"products_list":cache_items})
+    if cache_products:                                         # si hay resultado en el cache
+        return jsonify({"products_list":cache_products})       # arma el json y lo manda
 
     else:
         # es decir el string de la b ́usqueda es la llave y el valor es el resultado
--- src/cache.py
+++ src/cache.py
@@ -1,30 +1,29 @@
 from typing import ItemsView
 import redis
 import json
+PORT = 6379
+DB = 0
 
-def cache_load_json(key):
-    r = redis.Redis(host="localhost", port=6379, db=0)
+def cache_load_products(key):
+    r = redis.Redis(host="localhost", port=PORT, db=DB)  # se conecta a redis
     value = r.get(key)
-    print(value)
     if value:
-        return json.loads(value)
-    
+        return json.loads(value)    # retorna una lista con diccionario
     else:
-        return None
+        return None                 # retorna una vacio
 
-def cache_save_json(key, value_dict):
-    r = redis.Redis(host="localhost", port=6379, db=0)
-    r.set(key, json.dumps(value_dict))
+def cache_save_json(key, value):
+    r = redis.Redis(host="localhost", port=PORT, db=DB) # se conecta a redis
+    r.set(key, value)
 
 
 
 if __name__ == "__main__":
-    r = redis.Redis(host="localhost", port=6379, db=0)
     products= [
         {'name':'test', 'stock':22}, 
         {'name':'test bruh', 'stock':213123},
         {'name':'asddasdasd test', 'stock':3}
     ]
-    cache_save_json("test", products)
-    cache_items = cache_load_json("test")
+    cache_save_json("test", json.dumps(products))
+    cache_items = cache_load_products("test")
     print(f"cache: {cache_items}")
